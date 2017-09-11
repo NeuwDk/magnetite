5a41b3b62ee2af028b1d0049ee51fe6562899898
Daniel Neumann <danieln8600@gmail.com>
2017-09-11 09:27:25 +0000

    Server starts a TCPServer and listens for incoming requests

--- spec/magnetite_spec.cr
+++ spec/magnetite_spec.cr
@@ -1,11 +1,29 @@
 require "./spec_helper"
 
 describe Magnetite do
-  
-  describe Server do
+
+  describe Magnetite::Server do
+    host = "127.0.0.1"
+    port = 12345
+
+    server = Magnetite::Server.new(host, port)
+
+    describe "#initialize" do
+      it "saves assigned host and port" do
+        server.host.should eq(host)
+        server.port.should eq(port)
+      end
+    end
+
+    describe "#start" do
+      it "returns false as it sohuld already be started" do
+        server.start.should eq(false)
+      end
+    end
+
   end
 
-  describe Client do
+  describe Magnetite::Client do
   end
 
 end
--- src/magnetite.cr
+++ src/magnetite.cr
@@ -1,8 +1,44 @@
 require "./magnetite/*"
+require "socket"
 
 module Magnetite
 
+  # A Tuple Space server
+  #
+  #
   class Server
+    getter host : String, port : UInt64
+
+    def initialize(@host : String, port : Int)
+      @port = port.to_u64
+      @tcp = TCPServer.new(@host, @port)
+      @sockets = [] of TCPSocket
+
+      start
+    end
+
+    def start
+      return false if @spawned
+      @spawned = true
+
+      spawn do
+        loop do
+
+          if socket = @tcp.accept?
+            spawn handler(socket)
+          else
+            # server is closed
+            break
+          end
+
+        end
+      end
+    end
+
+    def handler(socket : TCPSocket)
+      @sockets << socket
+    end
+
   end
 
   class Client
