2a19252dad7f0fc2bfc68c8a1ef19a1293a1f3c7
Daniel Neumann <danieln8600@gmail.com>
2017-09-11 14:26:46 +0200

    moved Magnetite::Server to seperate file and same with tests

--- spec/magnetite_spec.cr
+++ spec/magnetite_spec.cr
@@ -2,34 +2,6 @@ require "./spec_helper"
 
 describe Magnetite do
 
-  describe Magnetite::Server do
-    host = "127.0.0.1"
-    port = 12345
-    server = Magnetite::Server.new(host, port)
-
-    describe "#initialize" do
-      it "saves assigned host and port" do
-        server.host.should eq(host)
-        server.port.should eq(port)
-      end
-
-      it "starts the tcp server" do
-        client = TCPSocket.new(host, port)
-        client << "ping\n"
-        client.gets.should eq("pong")
-        client.close
-      end
-
-    end
-
-    describe "#start" do
-      it "returns false as it sohuld already be started" do
-        server.start.should eq(false)
-      end
-    end
-
-  end
-
   describe Magnetite::Client do
   end
 
--- src/magnetite.cr
+++ src/magnetite.cr
@@ -1,50 +1,7 @@
-require "./magnetite/*"
 require "socket"
+require "./magnetite/*"
 
 module Magnetite
-
-  # A Tuple Space server
-  #
-  #
-  class Server
-    getter host : String, port : UInt64
-
-    def initialize(@host : String, port : Int)
-      @port = port.to_u64
-      @tcp = TCPServer.new(@host, @port)
-      @sockets = [] of TCPSocket
-
-      start
-    end
-
-    def start
-      return false if @spawned
-      @spawned = true
-
-      spawn do
-        loop do
-
-          if socket = @tcp.accept?
-            spawn handler(socket)
-          else
-            # server is closed
-            break
-          end
-
-        end
-      end
-    end
-
-    def handler(socket : TCPSocket)
-      @sockets << socket
-
-      loop do
-        msg = socket.gets
-        socket << "pong\n" if msg == "ping" # for testing
-      end
-    end
-
-  end
 
   class Client
   end
