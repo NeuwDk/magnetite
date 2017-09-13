82499621949f2877cbe38683e9b7b99a3a32742b
Daniel Neumann <danieln8600@gmail.com>
2017-09-13 09:08:38 +0000

    Implemented encode and decode on Magnetite::Protocol

--- spec/magnetite/protocol_spec.cr
+++ spec/magnetite/protocol_spec.cr
@@ -3,10 +3,60 @@ describe Magnetite::Protocol do
   describe ".parse" do
   end
 
+  describe ".stringify" do
+  end
+
   describe ".encode" do
+    it "encodes ',' to &0;" do
+      Magnetite::Protocol.encode(",").should eq("&0;")
+    end
+
+    it "encodes '[' to &1;" do
+      Magnetite::Protocol.encode("[").should eq("&1;")
+    end
+
+    it "encodes ']' to &2;" do
+      Magnetite::Protocol.encode("]").should eq("&2;")
+    end
+
+    it "encodes '&' to &3;" do
+      Magnetite::Protocol.encode("&").should eq("&3;")
+    end
+
+    it "encodes ';' to &4;" do
+      Magnetite::Protocol.encode(";").should eq("&4;")
+    end
+
+    it "encodes '\\n' to &5;" do
+      Magnetite::Protocol.encode("\n").should eq("&5;")
+    end
   end
 
   describe ".decode" do
+    it "decodes &0; to ','" do
+      Magnetite::Protocol.decode("&0;").should eq(",")
+    end
+
+    it "decodes &1; to '['" do
+      Magnetite::Protocol.decode("&1;").should eq("[")
+    end
+
+    it "decodes &2; to ']'" do
+      Magnetite::Protocol.decode("&2;").should eq("]")
+    end
+
+    it "decodes &3; to '&'" do
+      Magnetite::Protocol.decode("&3;").should eq("&")
+    end
+
+    it "decodes &4; to ';'" do
+      Magnetite::Protocol.decode("&4;").should eq(";")
+    end
+
+    it "decodes &5; to '\\n'" do
+      Magnetite::Protocol.decode("&5;").should eq("\n")
+    end
+
   end
 
 end
--- src/magnetite/protocol.cr
+++ src/magnetite/protocol.cr
@@ -3,13 +3,38 @@ module Magnetite
   module Protocol
     extend self
 
+    SPECIAL_SIGNS = [',', '[', ']', '&', ';', '\n']
+
     def parse(msg : String)
     end
 
-    def encode(obj)
+    def stringify(obj)
     end
 
+    # encodes a string to send over the socket
+    #
+    # *DON'T write "\*\n " in your string*
+    # it will fuck everything up!
+    def encode(obj : String)
+      out = obj.clone
+      SPECIAL_SIGNS.each_with_index do |v,i|
+        # replace v with "\*\#{i} " to not replace & and ; over and over
+        out = out.gsub(v, "\\*\\#{i} ")
+      end
+
+      # replace "\*\n " with "&n;"
+      out.gsub(/\\\*\\(?<n>[0-9]) /, "&\\k<n>;")
+    end
+
+    # decodes a string received from socket
     def decode(obj : String)
+      out = obj.clone
+      SPECIAL_SIGNS.each_with_index do |v,i|
+        # replace &#{i}; with #{v}
+        out = out.gsub("&#{i};", v)
+      end
+
+      out
     end
 
   end
