131e925d6449df22a8f10300ac1f0882f3f2ef09
Daniel Neumann <danieln8600@gmail.com>
2017-09-15 13:18:43 +0200

    Added Symbols to Type to use as a type identifier

--- src/magnetite.cr
+++ src/magnetite.cr
@@ -3,8 +3,8 @@ require "./magnetite/*"
 
 module Magnetite
 
-  alias Ints = Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32 | UInt64
-  #alias Types = Nil:Class, Bool:Class, Int:Class, Float:Class, String:Class, Array:Class
-  alias Type = Nil | Bool | Ints | Float64 | String | Array(Type)
+  private alias Ints = Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32 | UInt64
+  # Symbols represent types for when looking for a value of certain type
+  alias Type = Nil | Bool | Ints | Float64 | String | Array(Type) | Symbol
 
 end
