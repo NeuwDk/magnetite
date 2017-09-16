1234bc9ad1817f6dcb7af3d891d102b9bad275a1
Daniel Neumann <danieln8600@gmail.com>
2017-09-17 00:27:35 +0200

    Changed a Benchmark to try something out

--- benchmark/hash_lookup_sym_vs_type.cr
+++ benchmark/hash_lookup_sym_vs_type.cr
@@ -4,10 +4,17 @@ TYPE = {Nil => "n", Bool => "b", Int => "i", Float => "f", String => "s", Array 
 
 puts "get :float from SYM: #{SYM[:float]}"
 puts "get Float from TYPE: #{TYPE[Float]}"
+puts "get :float from SYM compile-time: #{ {{SYM[:float]}} }"
+puts "get Float from TYPE compile-time: #{ {{TYPE[Float]}} }"
 
 require "benchmark"
 
 Benchmark.ips do |x|
   x.report("get from SYM") { SYM[:string] }
   x.report("get from TYPE") { TYPE[String] }
+end
+
+Benchmark.ips do |x|
+  x.report("compiletime SYM") { {{SYM[:string]}} }
+  x.report("compiletime TYPE") { {{TYPE[String]}} }
 end
