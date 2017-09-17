449b3ed1c1e6fe341d70a6372cf3ed2dcbb0ebdd
Daniel Neumann <danieln8600@gmail.com>
2017-09-17 13:51:14 +0200

    Space specced out, no implementation yet
    Decided to ditch previous implementation.
    I want to implement it using a red-black tree to have it be efficient
    and O(log n) for most operations. I considered a binary tree, but I
    think having it being self sorting will be good for performance in the
    end.

--- spec/magnetite/space_spec.cr
+++ spec/magnetite/space_spec.cr
@@ -4,15 +4,16 @@ require "spec"
 describe Magnetite::Space do
   space = Magnetite::Space.new
   empty = [] of Magnetite::Type
+  tmp = [1,2,3] of Magnetite::Type
 
   describe "#write" do
     it "writes an array to the space" do
-      space.write(1,2,3)
+      space.write(tmp)
 
-      space.read_all.should eq([[1,2,3]])
+      space.read_all.should eq([tmp])
 
       #cleanup
-      space.take(1,2,3)
+      space.take(tmp)
       space.read_all.should eq(empty)
     end
 
@@ -23,36 +24,87 @@ describe Magnetite::Space do
       space.read_all.should eq(empty)
 
       #setup
-      space.write(1,2,3)
-      space.read_all.should eq([[1,2,3]])
+      space.write(tmp)
+      space.read_all.should eq([tmp])
 
-      space.take(1,2,3)
+      space.take(tmp)
       space.read_all.should eq(empty)
     end
 
     it "can get an array using nil as a wildcard" do
       space.read_all.should eq(empty)
 
-      space.write(1,2,3)
-      space.take(1,nil,3).should eq([1,2,3])
+      space.write(tmp)
+      space.take([1,:nil,3] of Magnetite::Type).should eq(tmp)
     end
 
     it "can get an array using a type as a selector" do
       space.read_all.should eq(empty)
 
-      space.write(1,2,3)
-      space.write(1,"2",3)
+      space.write(tmp)
+      space.write([1,"2",3] of Magnetite::Type)
 
-      space.take(1,String,3).should eq([1,"2",3])
-      space.take(1,Int,3).should eq([1,2,3])
+      space.take([1,:string,3]).should eq([1,"2",3])
+      space.take([1,:int,3]).should eq([1,2,3])
+    end
+
+    it "works with all supported types" do
+      space.write([false] of Magnetite::Type)
+      space.take([:bool]).should eq([false])
+
+      space.write([13] of Magnetite::Type)
+      space.take([:int]).should eq([13])
+
+      space.write([3.14] of Magnetite::Type)
+      space.take([:float]).should eq([3.14])
+
+      space.write(["s"] of Magnetite::Type)
+      space.take([:string]).should eq(["s"])
+
+      space.write([[1] of Magnetite::Type] of Magnetite::Type)
+      space.take([:array]).should eq([[1]])
+    end
+
+    it "works with nested wildcards in arrays" do
+      space.write([1,["s", 3.14] of Magnetite::Type] of Magnetite::Type)
+      space.take([:int, [:string, :float]]).should eq([1, ["s", 3.14]])
     end
 
   end
 
   describe "#read" do
+    # since the same search alg is used in read and take, I won't repeat tests
+
+    it "gets an array without removing it from the space" do
+      space.read_all.should eq(empty)
+
+      space.write(tmp)
+      space.read(tmp).should eq(tmp)
+
+      space.read_all.should eq([tmp])
+
+      #cleanup
+      space.take(tmp)
+    end
+
   end
 
   describe "#read_all" do
+    it "reads everything in the space" do
+      array = [] of Magnetite::Type
+
+      space.read_all.should eq(empty)
+
+      10.times do
+        space.write tmp
+        array << tmp
+      end
+
+      space.read_all.should eq(array)
+
+      #cleanup omitted as it's last test
+    end
+
   end
 
 end
--- src/magnetite/space.cr
+++ src/magnetite/space.cr
@@ -4,80 +4,28 @@ module Magnetite
   #
   # Still needs to be able to use nil as a wildcard to get anything
   # and types to get a value of that type
-  #
-  # Ideally I want to rewrite this to use a binary tree to search faster
   class Space
 
     def initialize
       @space = [] of Array(Type)
-      @mutex = Mutex.new
     end
 
     def write(array : Array(Type))
-      @mutex.synchronize do
-        @space << array
-      end
-      true
-    end
-    def write(*tuple)
-      write(tuple_to_array(tuple))
+      @space << array
     end
 
-    def take(array : Array(Type)) : Array(Type)
-      # use mutex when deleting from @space
-      id = find(array)
+    def take(array : Array(Type))
+      @space.shift?
 
-      @mutex.lock
-      if @space[id]?
-        out = @space.delete_at(id)
-        @mutex.unlock
-        out
-      else
-        @mutex.unlock
-        take(array)
-      end
-    end
-    def take(*tuple)
-      take(tuple_to_array(tuple))
+      array
     end
 
     def read(array : Array(Type))
-      id = find(array)
-
-      out = nil
-      @mutex.synchronize do
-        out = @space[id]?
-      end
-
-      out || read(array)
-    end
-    def read(*tuple)
-      read(tuple_to_array(tuple))
+      array
     end
 
     def read_all
       @space
-    end
-
-    private def find(array : Array(Type))
-      output = Channel(Int32).new
-
-      spawn do
-        @space.each_with_index do |v,i|
-          if v == array
-            output.send i
-            break
-          end
-        end
-        Fiber.yield
-      end
-
-      output.receive
-    end
-
-    def tuple_to_array(tuple)
-      array = [] of Type
-      tuple.each { |v| array.push(v) }
     end
 
   end
