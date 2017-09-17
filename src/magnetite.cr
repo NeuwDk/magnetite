d20ca00735f87a7bb5f615234227a69d20329d49
Daniel Neumann <danieln8600@gmail.com>
2017-09-17 08:19:46 +0200

    Added Types alias to Magnetite
    It's a Union Type of all the Types in their class form, so it can become
    possible to send a "tuple" with the type as a wildcard. Ex [Int, String]

--- src/magnetite.cr
+++ src/magnetite.cr
@@ -5,6 +5,7 @@ module Magnetite
 
   private alias Ints = Int8 | Int16 | Int32 | Int64 | UInt8 | UInt16 | UInt32 | UInt64
   # Symbols represent types for when looking for a value of certain type
-  alias Type = Nil | Bool | Ints | Float64 | String | Array(Type) | Symbol
+  private alias Types = Nil.class | Bool.class | Int.class | Float.class | String.class | Array.class
+  alias Type = Nil | Bool | Ints | Float64 | String | Array(Type) | Symbol | Types
 
 end
