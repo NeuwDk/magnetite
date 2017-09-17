886341fee50236233f6ee9f9889e1c8c0f09d92b
Daniel Neumann <danieln8600@gmail.com>
2017-09-17 09:17:49 +0200

    Changed the hash_lookup benchmark
    I wanted to see what would happen if I had a hash-map with both the
    symbols and the types, because it didn't work at compile-time using a
    type as a key.

--- benchmark/hash_lookup_sym_vs_type.cr
+++ benchmark/hash_lookup_sym_vs_type.cr
@@ -2,19 +2,28 @@ SYM = {:nil => "n", :bool => "b", :int => "i", :float => "f", :string => "s", :a
 
 TYPE = {Nil => "n", Bool => "b", Int => "i", Float => "f", String => "s", Array => "a", :type => "t"}
 
+BOTH = {
+  :nil => "n", :bool => "b", :int => "i", :float => "f", :string => "s", :array => "a", :type => "t",
+  Nil => "n", Bool => "b", Int => "i", Float => "f", String => "s", Array => "a"
+}
+
 puts "get :float from SYM: #{SYM[:float]}"
 puts "get Float from TYPE: #{TYPE[Float]}"
 puts "get :float from SYM compile-time: #{ {{SYM[:float]}} }"
 puts "get Float from TYPE compile-time: #{ {{TYPE[Float]}} }"
+puts "get :float from BOTH compile-time: #{ {{BOTH[:float]}} }"
 
 require "benchmark"
 
 Benchmark.ips do |x|
   x.report("get from SYM") { SYM[:string] }
   x.report("get from TYPE") { TYPE[String] }
+  x.report("get sym from BOTH") { BOTH[:string] }
+  x.report("get type from BOTH") { BOTH[String] }
 end
 
 Benchmark.ips do |x|
   x.report("compiletime SYM") { {{SYM[:string]}} }
   x.report("compiletime TYPE") { {{TYPE[String]}} }
+  x.report("compiletime sym from BOTH") { {{BOTH[:string]}} }
 end
