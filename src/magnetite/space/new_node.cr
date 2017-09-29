8938af8c1d3dd17b639daac30d4d2b03ef371af4
Daniel Neumann <danieln8600@gmail.com>
2017-09-29 10:10:34 +0000

    Made NewTree and NewNode
    They still thow an error I haven't had time to look at (when running
    Space's specs)

--- src/magnetite/space/new_node.cr
+++ src/magnetite/space/new_node.cr
@@ -2,11 +2,29 @@ module Magnetite
   class Space
 
     class NewNode
-      getter children : Array(Array(Type)), right : NewNode?, left : NewNode?, id : Int64
+      getter children : Array(Array(Type)), id : Int64
+      property right : NewNode?, left : NewNode?
 
       def initialize(val : Array(Type))
         @children = [val]
-        @id = val.first.hash
+        @id = val.first.hash.to_i64
+      end
+
+      def add(val : Array(Type))
+        @children << val
+      end
+
+      def to_a
+        out = [] of Type
+
+        r = right
+        l = left
+
+        out.concat(l.to_a) if l
+        out.concat(children)
+        out.concat(r.to_a) if r
+
+        out
       end
 
     end
--- src/magnetite/space/new_tree.cr
+++ src/magnetite/space/new_tree.cr
@@ -12,7 +12,131 @@ module Magnetite
           @root = NewNode.new(val)
         else
           # check if need to update existing node or create new
+          node = find_node(val)
+          id = val.first.hash
+
+          if node && node.id === id
+            node.add(val)
+          elsif node
+            # insert node at correct place
+            new_node = NewNode.new(val)
+
+            if node.id > id
+              node.left = new_node
+            else
+              node.right = new_node
+            end
+          end
         end
+      end
+      def <<(val : Array(Type))
+        insert(val)
+      end
+
+      # finds node, or the node that comes closest
+      #
+      # this is so that when inserting I don't loop over twice
+      # so it requires a check if the id matches
+      # `val.first.hash === find_node(val).id`
+      def find_node(val : Array(Type))
+        node = @root
+        if node
+          id = val.first.hash
+
+          loop do
+            if node.nil?
+              break
+            else
+              if node.id === id
+                return node
+              elsif node.id > id && node.left
+                node = node.left
+              elsif node.id < id && node.right
+                node = node.right
+              else
+                break
+              end
+            end
+          end
+
+          node
+        end
+      end
+
+      def find(val : Array(Type))
+        node = find_node(val)
+        id = val.first.hash
+
+        if node && node.id === id
+          children = node.children
+          out = nil
+          size = val.size
+
+          children.each do |v|
+            next unless size === v.size
+            match = true
+            index = 0
+
+            size.times do |i|
+              index = i
+
+              case val[i]
+              when v[i]
+                next
+              when Symbol
+                puts "wildcard!"
+                if val[i] === :nil
+                  next
+                elsif val[i] === :bool && v[i].is_a? Bool
+                  next
+                elsif val[i] === :int && v[i].is_a? Int
+                  next
+                elsif val[i] === :float && v[i].is_a? Float
+                  next
+                elsif val[i] === :string && v[i].is_a? String
+                  next
+                elsif val[i] === :array && v[i].is_a? Array
+                  next
+                else
+                  match = false
+                  break
+                end
+              else
+                match = false
+                break
+              end
+            end
+
+            if match
+              out = {index, node}
+              break
+            end
+          end
+
+          out
+        end
+      end
+
+      def read(val : Array(Type))
+        out = find(val)
+
+        if out
+          out[1].children[(out[0])]
+        end
+      end
+
+      def take(val : Array(Type))
+        out = find(val)
+
+        if out
+          out[1].children.delete_at (out[0])
+        end
+      end
+
+      def to_a
+        r = @root
+
+        r.to_a if r
       end
 
     end
