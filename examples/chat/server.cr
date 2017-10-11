36f9d44fa28c636e8839ad155de46e02a1ce950c
Daniel Neumann <danieln8600@gmail.com>
2017-10-11 10:14:01 +0000

    Changed the Chat example
    To avoid having too many messages stored in ChatServer, they are deleted
    after 5 seconds

--- examples/chat/server.cr
+++ examples/chat/server.cr
@@ -5,11 +5,21 @@ client = server.space
 
 client.write ["latest", 0] of Magnetite::Type
 
+def delete_msg(client, id : Int)
+  spawn do
+    sleep 5
+    client.take ["msg", id, String]
+  end
+end
+
 spawn do
   latest = 0
   loop do
-    _, _, msg = client.read ["msg", latest, :string]
+    _, _, msg = client.read ["msg", latest, String]
     puts "(#{latest}) #{msg}"
+
+    delete_msg(client, latest)
+
     latest = latest + 1
   end
 end
